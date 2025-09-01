c22bc8c493132fe6e1d54be16965e66a3ff7919e
Danilo <dznery@gmail.com>
2025-09-01 17:15:19 -0300

    Figured out config management

--- app/config.py
+++ app/config.py
@@ -1,28 +1,20 @@
 import os
-from functools import lru_cache
-from pydantic import Field, RedisDsn, PostgresDsn
+from functools import lru_cache, cached_property
+from pydantic import Field, RedisDsn, PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DevSettings(BaseSettings):
     # not strictly necessary since we explicitly set env_file in the compose file
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
-    # the following would allow setting ENV directly in the shell environment
-    # env: str = Field(default_factor=lambda: os.getenv("ENV", "dev"))
+    # the following would conveniently force validation of the ENV variable
+    #env: str = Field(default_factory=lambda: os.getenv("ENV", "dev"))
     env: str = "dev"
     debug: bool = True
-    # database creds
-    database_user: str = "arena"
-    database_pass: str = "arena"
-    database_host: str = "db"
-    database_port: str = "5432"
     # database dsn
-    database_url: PostgresDsn  # Required, but not defined here
-    # redis creds
-    redis_host: str = "redis"
-    redis_port: str = "6379"
+    database_url: PostgresDsn = "postgresql+asyncpg://arena:arena@db:5432/arena_dev"
     # redis dsn
-    database_url: PostgresDsn = f"postgresql+asyncpg://{database_user}:{database_pass}@{database_host}:{database_port}/arena_dev"
+    redis_url: RedisDsn = "redis://redis:6379/0"
     # admin "secret"
     admin_user: str = "admin"
     admin_pass: str = "youcantseeme"
@@ -34,24 +26,23 @@ class DevSettings(BaseSettings):
 class TestSettings(DevSettings):
     env: str = "test"
     debug: bool = True
-    database_url: PostgresDsn = f"postgresql+asyncpg://{database_user}:{database_pass}@{database_host}:{database_port}/arena_test"
+    database_url: PostgresDsn = "postgresql+asyncpg://arena:arena@db:5432/arena_test"
 
 
 class ProdSettings(DevSettings):
     env: str = "prod"
     debug: bool = False
-    database_url: PostgresDsn = f"postgresql+asyncpg://{database_user}:{database_pass}@{database_host}:{database_port}/arena"
+    database_url: PostgresDsn = "postgresql+asyncpg://arena:arena@db:5432/arena"
 
 
 @lru_cache
 def get_settings() -> BaseSettings:
-    defaults = DevSettings()
-    env_name = defaults.env.lower()
+    env_name = os.getenv("ENV", "dev").lower()
     return {
-            "dev": defaults,
+            "dev": DevSettings(),
             "test": TestSettings(),
             "prod": ProdSettings(),
-            }
+            }[env_name]
 
 
 settings = get_settings()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+from app.config import settings
+from rich import print
+
+
 def main():
-    print("Hello from session-arena!")
+    print(settings)
+    print(settings.redis_url)
+    print(settings.database_url)
 
 
 if __name__ == "__main__":
